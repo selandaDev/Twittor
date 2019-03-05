2eb4731f6af795ac6413bc3c13319c9dea35937b
Francisco Garcia <fmanosa23@gmail.com>
2019-03-05 12:18:07 +0100

    v3 cambios de estilo para IOS

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('js/sw-utils.js');
 
 // defs
 
-const STATIC_CACHE = 'static-v2';
+const STATIC_CACHE = 'static-v3';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
